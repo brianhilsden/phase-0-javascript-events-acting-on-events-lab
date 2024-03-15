b23e5f90ea02a6f2f8ebdec1addebd5c78261155
Brian Omondi <brianhilsden@gmail.com>
2024-03-15 17:49:20 +0300

    Moved div with event listener

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+const dodger=document.getElementById("dodger")
+
+
+function moveDodgerLeft(){
+    const left=dodger.style.left.replace("px","")
+    const leftInt=parseInt(left,10)
+    if (leftInt>0){
+        dodger.style.left=`${leftInt-1}px`}
+
+}
+function moveDodgerRight(){
+    const left=dodger.style.left.replace("px","")
+    const leftInt=parseInt(left,10)
+    if (leftInt<360){
+        dodger.style.left=`${leftInt+1}px`
+    }
+
+}
+document.addEventListener("keydown",function(e){
+    if(e.key==="ArrowLeft"){
+        moveDodgerLeft()
+        
+    }
+    else if(e.key==="ArrowRight"){
+        moveDodgerRight()
+        
+        
+    }
+    }
+    )
